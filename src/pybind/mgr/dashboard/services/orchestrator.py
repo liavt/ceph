sunilangadi2/ceph-ci

cephadm: expose gather-facts api method
Fixes: https://tracker.ceph.com/issues/51209

This PR intends to expose host metadata(gather-facts) api method

Signed-off-by: Avan Thakkar <athakkar@redhat.com>
Signed-off-by: Aashish Sharma <aasharma@redhat.com>

src/pybind/mgr/dashboard/services/orchestrator.py
@@ -63,6 +63,10 @@ class HostManger(ResourceManager):
         return hosts[0] if hosts else None
 
     @wait_api_result
+    def get_gather_facts(self, host: Optional[str] = None) -> List[Dict[str, Any]]:
+        return self.api.get_gather_facts(host)
+
+    @wait_api_result
     def add(self, hostname: str, addr: str, labels: List[str], status: str):
         return self.api.add_host(HostSpec(hostname, addr=addr, labels=labels, status=status))
 

src/pybind/mgr/orchestrator/_interface.py
@@ -368,6 +368,12 @@ class Orchestrator(object):
         """
         raise NotImplementedError()
 
+    def get_gather_facts(self, hostname: Optional[str] = None) -> OrchResult[List[Dict[str, Any]]]:
+        """
+        Return hosts metadata(gather_facts).
+        """
+        raise NotImplementedError()
+
     def add_host_label(self, host: str, label: str) -> OrchResult[str]:
         """
         Add a host label

liavt/ceph

src/pybind/mgr/dashboard/services/orchestrator.py
@@ -67,6 +67,10 @@ class HostManger(ResourceManager):
         return self.api.add_host(HostSpec(hostname, addr=addr, labels=labels))
 
     @wait_api_result
+    def get_facts(self, hostname: Optional[str] = None) -> List[Dict[str, Any]]:
+        return self.api.get_facts(hostname)
+
+    @wait_api_result
     def remove(self, hostname: str):
         return self.api.remove_host(hostname)
 

src/pybind/mgr/orchestrator/_interface.py
@@ -376,6 +376,12 @@ class Orchestrator(object):
         """
         raise NotImplementedError()
 
+    def get_facts(self, hostname: Optional[str] = None) -> OrchResult[List[Dict[str, Any]]]:
+        """
+        Return hosts metadata(gather_facts).
+        """
+        raise NotImplementedError()
+
     def add_host_label(self, host: str, label: str) -> OrchResult[str]:
         """
         Add a host label