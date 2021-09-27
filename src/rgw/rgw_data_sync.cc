8e2c9fbfe872fc4f6ad32d7e6acda0216909ad59
liavt <liav.turkia@gmail.com>
2021-09-27 21:50:18 +0300

    fixed indentation

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -2342,48 +2342,48 @@ public:
           }
         }
 
-		{
-			/*
-			 * we need to fetch info about source object, so that we can determine
-			 * the correct policy configuration. This can happen if there are multiple
-			 * policy rules, and some depend on the object tagging */
-			yield call(new RGWStatRemoteObjCR(sync_env->async_rados,
-											  sync_env->store,
-											  sc->source_zone,
-											  sync_pipe.info.source_bs.bucket,
-											  key,
-											  &src_mtime,
-											  &src_size,
-											  &src_etag,
-											  &src_attrs,
-											  &src_headers));
-			if (retcode < 0) {
-			  return set_cr_error(retcode);
-			}
+        {
+            /*
+             * we need to fetch info about source object, so that we can determine
+             * the correct policy configuration. This can happen if there are multiple
+             * policy rules, and some depend on the object tagging */
+            yield call(new RGWStatRemoteObjCR(sync_env->async_rados,
+                                              sync_env->store,
+                                              sc->source_zone,
+                                              sync_pipe.info.source_bs.bucket,
+                                              key,
+                                              &src_mtime,
+                                              &src_size,
+                                              &src_etag,
+                                              &src_attrs,
+                                              &src_headers));
+            if (retcode < 0) {
+              return set_cr_error(retcode);
+            }
 
-			
+            
 
-			auto iter = src_attrs.find(RGW_ATTR_TAGS);
-			if (iter != src_attrs.end()) {
-			  try {
-				auto it = iter->second.cbegin();
-				obj_tags.decode(it);
-			  } catch (buffer::error &err) {
-				ldout(cct, 0) << "ERROR: " << __func__ << ": caught buffer::error couldn't decode TagSet " << dendl;
-			  }
-			}
+            auto iter = src_attrs.find(RGW_ATTR_TAGS);
+            if (iter != src_attrs.end()) {
+              try {
+                auto it = iter->second.cbegin();
+                obj_tags.decode(it);
+              } catch (buffer::error &err) {
+                ldout(cct, 0) << "ERROR: " << __func__ << ": caught buffer::error couldn't decode TagSet " << dendl;
+              }
+            }
 
-			rgw_sync_pipe_params params;
-			if (!sync_pipe.info.handler.find_obj_params(key,
-														obj_tags.get_tags(),
-														&params)) {
-			  return set_cr_error(-ERR_PRECONDITION_FAILED);
-			}
-			param_user = params.user;
-			param_mode = params.mode;
+            rgw_sync_pipe_params params;
+            if (!sync_pipe.info.handler.find_obj_params(key,
+                                                        obj_tags.get_tags(),
+                                                        &params)) {
+              return set_cr_error(-ERR_PRECONDITION_FAILED);
+            }
+            param_user = params.user;
+            param_mode = params.mode;
 
-			dest_params = params.dest;		
-	    }
+            dest_params = params.dest;        
+        }
 
 
         if (param_mode == rgw_sync_pipe_params::MODE_USER) {
@@ -2450,31 +2450,31 @@ public:
         }
         
         // notify that object has synced to this zone
-		
+        
         rgw::sal::RadosObject obj(sync_env->store, key);
         rgw::sal::RadosBucket bucket(sync_env->store, sync_pipe.info.source_bs.bucket);
         RGWObjectCtx rctx(sync_env->store);
-		std::string user_id = "0";
-		std::string req_id = "rgw sync";
-		std::string tenant(bucket.get_tenant());
+        std::string user_id = "0";
+        std::string req_id = "rgw sync";
+        std::string tenant(bucket.get_tenant());
         std::unique_ptr<rgw::sal::Notification> notify 
-		         = sync_env->store->get_notification(dpp, &obj, nullptr, &rctx, rgw::notify::ObjectSyncedCreate,
-			      &bucket, user_id,
-			      tenant,
-			      user_id, null_yield);
-				  
-		
+                 = sync_env->store->get_notification(dpp, &obj, nullptr, &rctx, rgw::notify::ObjectSyncedCreate,
+                  &bucket, user_id,
+                  tenant,
+                  user_id, null_yield);
+                  
+        
         auto notify_res = static_cast<rgw::sal::RadosNotification*>(notify.get())->get_reservation();
         int ret = rgw::notify::publish_reserve(dpp, rgw::notify::ObjectSyncedCreate, notify_res, &obj_tags);
         if (ret < 0) {
           ldpp_dout(dpp, 1) << "ERROR: reserving notification failed, with error: " << ret << dendl;
-		  // no need to return, the sync already happened
-		} else {
+          // no need to return, the sync already happened
+        } else {
           ret = rgw::notify::publish_commit(&obj, src_size, src_mtime, src_etag, 0/* version id */, rgw::notify::ObjectSyncedCreate, notify_res, dpp);
           if (ret < 0) {
             ldpp_dout(dpp, 1) << "ERROR: publishing notification failed, with error: " << ret << dendl;
-		  }
-		}
+          }
+        }
 
         return set_cr_done();
       }
